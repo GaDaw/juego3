1632cfe82150b6788c2ea47b6c013a261fa8761e
Diego Ponce de León <malandro@gmail.com>
2017-11-21 19:16:12 +0100

    first commit

--- FightGame/Program.cs
+++ FightGame/Program.cs
@@ -6,7 +6,11 @@ namespace FightGame
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.Title = '\u263A'.ToString();
+            System.Console.WriteLine('\u263B');
+
+            var game = new Game();
+            game.Start();
         }
     }
 }
