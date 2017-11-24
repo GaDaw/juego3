497893f4933dad27782fc4ebf63123e70235576c
Diego Ponce de León <malandro@gmail.com>
2017-11-24 08:35:35 +0100

    Actualizar CustomPlayerService para que implemente todos los métodos

--- FightGame.Services/CustomPlayerService.cs
+++ FightGame.Services/CustomPlayerService.cs
@@ -1,47 +1,86 @@
 ﻿using FightGame.Model;
+using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace FightGame
 {
     public class CustomPlayerService : IPlayerService
     {
+        private static List<Player> _players = new List<Player>();
+
+        public Player AddPlayer(Player player)
+        {
+            player.Id = _players.Max(x => x.Id) + 1;
+            _players.Add(player);
+
+            return player;
+        }
+
+        public Player GetPlayerById(int id)
+        {
+            return _players.First(x => x.Id == id);
+        }
+
         public List<Player> GetPlayers()
         {
-            return new List<Player>
+            if (!_players.Any())
             {
-                new Player
+                _players = new List<Player>
                 {
-                    Id = 1,
-                    Name = "Cat Woman",
-                    Gender = Gender.Female,
-                    Lives = GameModel.DefaultLives,
-                    Power = GameModel.DefaultPower
-                },
-                new Player
-                {
-                    Id = 2,
-                    Name = "Lobezno",
-                    Gender = Gender.Male,
-                    Lives = GameModel.DefaultLives,
-                    Power = GameModel.DefaultPower
-                },
-                new Player
-                {
-                    Id = 3,
-                    Name = "Wonder Woman",
-                    Gender = Gender.Female,
-                    Lives = GameModel.DefaultLives,
-                    Power = GameModel.DefaultPower
-                },
-                new Player
-                {
-                    Id = 4,
-                    Name = "Batman",
-                    Gender = Gender.Male,
-                    Lives = GameModel.DefaultLives,
-                    Power = GameModel.DefaultPower
-                },
-            };
+                    new Player
+                    {
+                        Id = 1,
+                        Name = "Cat Woman",
+                        Gender = Gender.Female,
+                        Lives = GameModel.DefaultLives,
+                        Power = GameModel.DefaultPower
+                    },
+                    new Player
+                    {
+                        Id = 2,
+                        Name = "Lobezno",
+                        Gender = Gender.Male,
+                        Lives = GameModel.DefaultLives,
+                        Power = GameModel.DefaultPower
+                    },
+                    new Player
+                    {
+                        Id = 3,
+                        Name = "Wonder Woman",
+                        Gender = Gender.Female,
+                        Lives = GameModel.DefaultLives,
+                        Power = GameModel.DefaultPower
+                    },
+                    new Player
+                    {
+                        Id = 4,
+                        Name = "Batman",
+                        Gender = Gender.Male,
+                        Lives = GameModel.DefaultLives,
+                        Power = GameModel.DefaultPower
+                    },
+                };
+            }
+
+            return _players;
+        }
+
+        public Player UpdatePlayer(Player player)
+        {
+            var match = _players.FirstOrDefault(x => x.Id == player.Id);
+
+            if(match != null)
+            {
+                match.Lives = player.Lives;
+                match.Power = player.Power;
+                match.Name = player.Name;
+                match.Gender = player.Gender;
+
+                return match;
+            }
+
+            throw new Exception("Jugador no encontrado");
         }
     }
 }
