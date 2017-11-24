6ce2200402499b5298fc5d58b93b7dc8cecba495
Diego Ponce de León <malandro@gmail.com>
2017-11-24 08:35:12 +0100

    Completar la interfaz IPlayerService

--- FightGame.Services/IPlayerService.cs
+++ FightGame.Services/IPlayerService.cs
@@ -5,5 +5,11 @@ namespace FightGame
     public interface IPlayerService
     {
         List<Player> GetPlayers();
+
+        Player GetPlayerById(int id);
+
+        Player AddPlayer(Player player);
+
+        Player UpdatePlayer(Player player);
     }
 }
