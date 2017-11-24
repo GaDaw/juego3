6c7aef9300b725c2e02a5fa03b086eb44ac78f1c
Diego Ponce de León <malandro@gmail.com>
2017-11-24 08:44:23 +0100

    Arreglar StartwarsPlayerService.GetPlayers para que mantenga el estado

--- FightGame.Services/StarwarsPlayerService.cs
+++ FightGame.Services/StarwarsPlayerService.cs
@@ -4,7 +4,6 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http;
-using System.Threading.Tasks;
 
 namespace FightGame
 {
@@ -29,44 +28,26 @@ namespace FightGame
 
         public List<Player> GetPlayers()
         {
-            var httpClient = new HttpClient();
-            Task<string> task = httpClient.GetStringAsync(ApiUrl);
-
-            // forma 1 de recuperar valor
-            //Task.Run(async() => {
-            //    string result = await task;
-            //});
-
-            // forma 2 de recuperar valor
-            string result = task.Result;
-            StartWarsPeople people = JsonConvert.DeserializeObject<StartWarsPeople>(result);
-
-            // 1: Convertir List<Person> en nuestro List<Player> con un bucle
-            //var players = new List<Player>();
-            //foreach(var person in people.results)
-            //{
-            //    players.Add(new Player
-            //    {
-            //        Id = ++Game.LastId,
-            //        Name = person.name,
-            //        Gender = person.gender == "male" ? Gender.Male : Gender.Female,
-            //        Lives = Game.DefaultLives,
-            //        Power = Game.DefaultPower
-            //    });
-            //}
-
-            // 2: Convertir List<Person> en nuestro List<Player> con LINQ
-            var lastId = 0;
-            var players = people.Results.Select(person => new Player
+            if(!_players.Any())
             {
-                Id = ++lastId,
-                Name = person.PlayerName,
-                Gender = person.PlayerGender == "male" ? Gender.Male : Gender.Female,
-                Lives = GameModel.DefaultLives,
-                Power = GameModel.DefaultPower
-            });
+                var httpClient = new HttpClient();
+                var result = httpClient.GetStringAsync(ApiUrl).Result;
+                StartWarsPeople people = JsonConvert.DeserializeObject<StartWarsPeople>(result);
 
-            return players.ToList();
+                var lastId = 0;
+                var players = people.Results.Select(person => new Player
+                {
+                    Id = ++lastId,
+                    Name = person.PlayerName,
+                    Gender = person.PlayerGender == "male" ? Gender.Male : Gender.Female,
+                    Lives = GameModel.DefaultLives,
+                    Power = GameModel.DefaultPower
+                });
+
+                _players = players.ToList();
+            }
+
+            return _players;
         }
 
         public Player UpdatePlayer(Player player)
